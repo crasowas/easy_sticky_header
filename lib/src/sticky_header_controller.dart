7af8fc2d2d995cddeaa1007b50dd1f7ed74084f7
crasowas <110958409+crasowas@users.noreply.github.com>
2024-02-23 18:03:14 +0800

    Fix scroll position issue in animateTo() method

--- lib/src/sticky_header_controller.dart
+++ lib/src/sticky_header_controller.dart
@@ -322,7 +322,7 @@ class StickyHeaderController extends ChangeNotifier {
   }) {
     var stickyHeaderInfo = getStickyHeaderInfo(index);
     if (stickyHeaderInfo != null) {
-      var pixels = _calculatePixels(stickyHeaderInfo, offset);
+      var pixels = min(getMaxScrollExtent, _calculatePixels(stickyHeaderInfo, offset));
       if (currentPixels != pixels) {
         isJumping = true;
         _scrollPosition?.animateTo(
